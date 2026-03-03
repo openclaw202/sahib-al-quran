5b540522ca5bc158276975835c0bd709751353e7
Clawd <claw@Mac-mini-Abdelrahman.local>
2026-03-04 00:07:12 +0200

    Update project structure for Vite and GitHub Pages

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainDashboard } from './features/quran/components/MainDashboard'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MainDashboard />
+    </div>
+  )
+}
+
+export default App
